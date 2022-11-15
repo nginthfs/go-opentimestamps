dd7a398983c10cf0d7119c3f04b31bd6744ea18e
Nick Ginther <104029658+nginthfs@users.noreply.github.com>
2022-11-15 10:28:27 -0600

    Update bitcoin_test.go

--- opentimestamps/client/bitcoin_test.go
+++ opentimestamps/client/bitcoin_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/blockchainsource/go-opentimestamps/opentimestamps"
+	"github.com/nginthfs/go-opentimestamps/opentimestamps"
 	btcrpcclient "github.com/btcsuite/btcd/rpcclient"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
